551eb922f57b9e05c56c848a488477c15d2de543
LiLaVanRAW <Alexmausy1990@freenet.de>
2014-10-30 10:06:32 +0100

    Create breitenSuche.h

---
+++ breitenSuche.h
@@ -0,0 +1,11 @@
+#ifndef BREITENSUCHE_H
+#define BREITENSUCHE_H
+
+/*************** FUNKTIONSDEKLARATIONEN ****************/
+int breitenSuche(int startPunkt);
+void pushKnoten(int aktKnoten);
+int getKnoten();
+void display(char a);
+/*************** FUNKTIONSDEKLARATIONEN ****************/
+
+#endif
